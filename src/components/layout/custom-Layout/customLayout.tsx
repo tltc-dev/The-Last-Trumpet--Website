2990328563bf6858de3e81858504b5169221fb1b
ANJITH B <anjithb200@gmail.com>
2025-09-30 21:06:43 +0530

    refactor(layout): improve custom layout and add providers

--- src/components/layout/custom-Layout/customLayout.tsx
+++ src/components/layout/custom-Layout/customLayout.tsx
@@ -18,7 +18,7 @@ const CustomLayout = ({ children, className }: Props) => {
   const scrollRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const lenis = new Lenis({ lerp: 0.1, smoothWheel: true });
+    const lenis = new Lenis({ lerp: 1, smoothWheel: true });
     function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
